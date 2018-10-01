5f31291ccaa38bbf21e52f4de144d6c846da6e05
poldrack <poldrack@gmail.com>
2018-10-01 13:26:33 -0700

    adding local mac setup instructions

--- setup/README.md
+++ setup/README.md
@@ -11,7 +11,7 @@ To use the VM, first set up the following software on your computer:
 
 - Install [Vagrant](https://www.vagrantup.com/)
     - Windows users will likely need to [turn off Hyper-V](https://ugetfix.com/ask/how-to-disable-hyper-v-in-windows-10/)
-    
+
 - Install [git](https://git-scm.com/) if you don't already have it.
 
 ### Creating your own fork of the class repository.
@@ -74,3 +74,16 @@ Once you have done this it should pull down the new Vagrantfile, and you can the
 
 - I find the default screensaver to use up a lot of processing power, so I generally change it to a blank screen (under Preferences->Screensaver).
 - You can add an ssh key for your VM so that you won't have to enter your github credentials each time you push or pull.  See [here](https://help.github.com/articles/adding-a-new-ssh-key-to-your-github-account/) for info on how to do this.  
+
+
+## Alternative: Local installation
+
+Some people have had trouble installing the virtual machine on their systems, in which case it may be preferable to install the software locally.  Here are the steps for installing on a Mac.  For Windows users, I would suggest sticking with the VM or installing the Windows Linux Subsystem (which requires Windows 10 Professional):
+
+1.  Download and install the [Anaconda](https://www.anaconda.com/download/) Python distribution.
+2. Go to your local clone of this reposistory, and pull the latest version: ```git pull upstream master```
+3. Execute the setup script: ```sh setup/local_setup_mac.sh```
+
+This will install a new python virtual environment called py36 - whenever you want to use this in a new session you will first need to activate the virtual environment using ```source activate py36```
+
+All downloaded software will be installed in ~/brain-networks-software
---
+++ setup/local_setup_mac.sh
@@ -0,0 +1,63 @@
+# this setup file adapted from the Vagrantfile for local installation
+
+BASEDIR=$HOME/brain-networks-software
+
+if [ ! -d $BASEDIR ]
+then
+    mkdir $BASEDIR
+fi
+
+conda update --yes conda
+conda create --yes -n py36 python=3.6 anaconda
+
+source activate py36
+
+conda install --yes -c anaconda biopython
+conda install --yes -c conda-forge python-igraph
+conda install --yes -c conda-forge nibabel
+conda install --yes -c conda-forge nilearn
+conda install --yes -c conda-forge nipy
+conda install --yes -c conda-forge xlrd
+
+pip install --upgrade https://github.com/nipy/nipype/archive/master.zip
+conda install --yes -c conda-forge dipy
+conda install --yes vtk
+
+pip install SimpleITK
+
+cd $BASEDIR
+git clone https://github.com/aestrivex/bctpy.git
+cd bctpy
+python setup.py install
+cd
+
+
+echo "downloading SPM..."
+cd $BASEDIR
+wget https://www.fil.ion.ucl.ac.uk/spm/download/restricted/utopia/spm12_r7219.zip -O spm12.zip
+unzip spm12.zip
+rm -rf spm12.zip
+cd
+
+# get connectome workbench
+echo "downloading connectome workbench"
+cd $BASEDIR
+wget https://ftp.humanconnectome.org/workbench/workbench-mac64-v1.3.2.zip
+unzip workbench-mac64-v1.3.2.zip
+
+
+echo "downloading tetrad"
+cd $BASEDIR
+wget --quiet  http://www.phil.cmu.edu/projects/tetrad_download/download/tetrad-4.3.10-7.jar
+
+
+# install FSLeyes
+cd $BASEDIR
+wget https://fsl.fmrib.ox.ac.uk/fsldownloads/fsleyes/FSLeyes-latest-macos.zip
+unzip FSLeyes-latest-macos.zip
+
+echo "downloading MATLAB runtime"
+echo "Installer will open automatically"
+wget -nc --quiet http://ssd.mathworks.com/supportfiles/downloads/R2018b/deployment_files/R2018b/installers/maci64/MCR_R2018b_maci64_installer.dmg.zip -O $HOME/Downloads/MCR_R2018b_maci64_installer.dmg.zip
+unzip $HOME/Downloads/MCR_R2018b_maci64_installer.dmg.zip -d $HOME/Downloads
+open $HOME/Downloads/MCR_R2018b_maci64_installer.dmg
